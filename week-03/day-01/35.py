e71d8745993f77b23201034db7ffd9accf687fe7
zsolt-fekete <zsoltfekete.zsf@gmail.com>
2016-05-18 13:13:03 +0200

    For and While practise

---
+++ week-03/day-01/35.py
@@ -0,0 +1,17 @@
+#while
+ah = ['kuty', 'macsk', 'cic']
+
+i = 0
+b = len(ah)
+
+while i < b :
+    ah[i] += (ah[i]+"a")
+    i += 1
+print(ah)
+
+#for
+ah = ['kuty', 'macsk', 'cic']
+
+for i in  range(len(ah)):
+    ah[i] += "a"
+print(ah
---
+++ week-03/day-01/36.py
@@ -0,0 +1,18 @@
+ah = [3, 4, 5, 6, 7]
+
+b = sum(ah)
+print(b)
+
+#while
+i=0
+summa=0
+
+while 1 < len(ah):
+    summa += ah[i]
+    i += 1
+
+#for
+
+for szam in ah
+    summa += szam
+print (summa) 
