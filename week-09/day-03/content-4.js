702caa847ed92241801c4cd85d5ff98aa4531010
zsolt-fekete <zsoltfekete.zsf@gmail.com>
2016-06-29 12:02:00 +0200

    1-5 exercise

---
+++ week-09/day-03/content-4.js
@@ -0,0 +1,4 @@
+  // replace the list items' content with items from this list:
+var list = ['apple', 'banana', 'cat', 'dog']
+var li = document.querySelectorAll('li');
+li.forEach(function(e,i) {e.innerHTML = list[i]});
